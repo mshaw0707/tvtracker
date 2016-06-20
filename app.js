6a871cd0fb070c82d40fbe54ffd42ae6cc954e8f
Mathew Shaw <mshaw0707@gmail.com>
2016-06-20 14:49:54 -0700

    Added handlebars template engine and main view

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
 var app = require('express')();
+var hbs = require('express-handlebars');
 
 app.set('port', process.env.PORT || 3000);
 
+app.engine('.hbs', hbs({defaultLayout: 'main', extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
 app.get('/', function(req, res) {
-  res.type('text/plain');
-  res.send('TV Tracker');
+  res.render('home');
 })
 
 app.use(function(req, res) {
-          res.type('text/plain');
-          res.status(404);
-          res.send('404 - Not Found');
+  res.type('text/plain');
+  res.status(404);
+  res.send('404 - Not Found');
 });
 
 app.use(function(err, req, res, next) {
-          console.error(err.stack);
-          res.type('text/plain');
-          res.status(500);
-          res.send('500 - Server Error');
+  console.error(err.stack);
+  res.type('text/plain');
+  res.status(500);
+  res.send('500 - Server Error');
 });
 
 app.listen(app.get('port'), function() {
