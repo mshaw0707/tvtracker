9ca30be98b0ecc88aff19f5e7e79219000061231
Mathew Shaw <mshaw0707@gmail.com>
2016-06-20 15:05:09 -0700

    added public folder and image to main layout

--- app.js
+++ app.js
@@ -1,26 +1,29 @@
-var app = require('express')();
+var express = require('express');
 var hbs = require('express-handlebars');
+
+var app = express();
 
 app.set('port', process.env.PORT || 3000);
 
 app.engine('.hbs', hbs({defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(req, res) {
   res.render('home');
 })
 
 app.use(function(req, res) {
-  res.type('text/plain');
   res.status(404);
-  res.send('404 - Not Found');
+  res.render('404');
 });
 
 app.use(function(err, req, res, next) {
   console.error(err.stack);
   res.type('text/plain');
   res.status(500);
-  res.send('500 - Server Error');
+  res.render('500');
 });
 
 app.listen(app.get('port'), function() {
